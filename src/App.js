5365768e63b0e872b3278fca3431bc6f3ae42c2f
Jeong Eunwoo <zenic88@naver.com>
2018-07-01 21:54:08 +0900

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Game from './components/Game';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+        <Game />
+    );
+  }
+}
+
+export default App;
